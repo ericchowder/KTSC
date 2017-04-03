6c9a2cfcda88cc38bcab7e95a1fedb0726cb87de
kevinkid135 <kevinkid228@hotmail.com>
2017-04-03 16:16:19 -0400

    added Homepage

---
+++ 332Project/HomePage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Registration Page</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<h1>KTSC</h1>
+<div class="corner">
+    <a href="index.php">logout</a>
+</div>
+<p>
+    <a href="parkingLocations.php">Parking Locations</a>
+</p>
+<p>
+    <a href="rentalHistory.php">Rental History</a>
+</p>
+<p>Available cars on <input type="date" name="date">
+    <button>Search</button>
+</p>
+</body>
