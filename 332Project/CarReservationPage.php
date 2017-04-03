dba8ed04d5aed6ab8c6f5cdceaa8765676465815
kevinkid135 <kevinkid228@hotmail.com>
2017-04-03 17:43:09 -0400

    Created comments Page

--- 332Project/CarReservationPage.php
+++ 332Project/CarReservationPage.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-<h1>TEMP_CAR_TYPE</h1>
+<h1 class="carTitle">TEMP_CAR_TYPE</h1>
 <p>Price: $<span id="carPrice"></span> for <input type="number" value="1" min="1"> days</p>
 <p>Total: <span id="total"></span></p>
 <input type="button" value="Rent">
---
+++ 332Project/comments.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Reservation</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<h1 class="carTitle">CAR COMMENTS</h1>
+<?php
+
+?>
+</body>
+</html>
