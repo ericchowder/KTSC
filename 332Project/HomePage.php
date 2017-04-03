07381d8c2255675eba83d006400f252befc92d0d
paolokiller <lazzari.paolok@gmail.com>
2017-04-03 15:27:35 -0700

    Update HomePage.php

--- 332Project/HomePage.php
+++ 332Project/HomePage.php
@@ -1,10 +1,26 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>HomePage Page</title>
+    <title>Homepage</title>
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
+<body>
+ <?php
+  //Create a user session or resume an existing one
+ session_start();
+ ?>
+ 
+  <?php
+ //check if the user clicked the logout link and set the logout GET parameter
+if(isset($_GET['logout'])){
+	//Destroy the user's session.
+	$_SESSION['id']=null;
+	header("Location: index.php");
+	session_destroy();
+}
+ ?>
+
 <h1>KTSC</h1>
 <div class="corner">
     <a href="index.php">logout</a>
@@ -19,4 +35,3 @@
     <button>Search</button>
 </p>
 </body>
-</html>
