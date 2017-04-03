3e04176ed8cc9bfe65626033c047178059c3af64
paolokiller <lazzari.paolok@gmail.com>
2017-04-03 16:03:34 -0700

    Update registration.php

--- 332Project/registration.php
+++ 332Project/registration.php
@@ -19,8 +19,9 @@ if(isset($_POST['regBtn'])){
 	
 	if(!$con)
 	{
-		die("Could not connect:".mysql_error());
+		die("Could not connect:".mysqli_error());
 	}
+	
 	
 	$aptNum = $_POST['aptNum'];
 	$city = $_POST['city'];
@@ -39,18 +40,15 @@ if(isset($_POST['regBtn'])){
 	first_name,last_name,phone_number,state,street_name,street_no,zip_code)".
 	"VALUES ('$aptNum','$city','$driving','$email','$first','$second','$phone','$state','$streetName','$streetNum','$zip')";
 	
-	mysql_select_db('project');
-	$retval=mysql_query($sql,$con);
-	
-	if(!$retval){
-		die('Could not enter data:' .mysql_error());
+
+	if(!$con){
+		die('Could not enter data:' .mysqli_error());
 	}
 	
-	echo "Entered data successfully";
+	mysqli_select_db($con,"project") or die (mysqli_error());
+
 	
-	mysql_query($sql);
-	
-	mysqli_close($con);
+	($con->close());
  }
 ?>
 
@@ -63,77 +61,77 @@ if(isset($_POST['regBtn'])){
         <table border='0'>
             <tr>
                 <td>Username</td>
-                <td><input type='text' name='username' id='username'/></td>
+                <td><input type='text' name='username' id='username' required//></td>
             </tr>
 
             <tr>
                 <td>Password</td>
-                <td><input type='password' name='password' id='password'/></td>
+                <td><input type='password' name='password' id='password' required//></td>
             </tr>
 
             <tr>
                 <td>First name</td>
-                <td><input type='text' name='first' id='first'/></td>
+                <td><input type='text' name='first' id='first'required//></td>
             </tr>
 
             <tr>
                 <td>Last name</td>
-                <td><input type='text' name='second' id='second'/></td>
+                <td><input type='text' name='second' id='second'required//></td>
             </tr>
 
             <tr>
                 <td>Email</td>
-                <td><input type='text' name='email' id='email'/></td>
+                <td><input type='text' name='email' id='email'required//></td>
             </tr>
 
             <tr>
                 <td>Driver's license Number</td>
-                <td><input type='text' name='driving' id='driving'/></td>
+                <td><input type='text' name='driving' id='driving'required//></td>
             </tr>
 
             <tr>
                 <td>Phone number</td>
-                <td><input type='text' name='phone' id='phone'/></td>
+                <td><input type='text' name='phone' id='phone'required//></td>
             </tr>
 
             <tr>
                 <td>Street Number</td>
-                <td><input type='text' name='streetNum' id='streetNum'/></td>
+                <td><input type='text' name='streetNum' id='streetNum'required//></td>
             </tr>
 
             <tr>
                 <td>Street Name</td>
-                <td><input type='text' name='streetName' id='streetName'/></td>
+                <td><input type='text' name='streetName' id='streetName'required//></td>
             </tr>
 
             <tr>
                 <td>Apt Number</td>
-                <td><input type='text' name='aptNum' id='aptNum'/></td>
+                <td><input type='text' name='aptNum' id='aptNum'required//></td>
             </tr>
 
             <tr>
                 <td>City</td>
-                <td><input type='text' name='city' id='city'/></td>
+                <td><input type='text' name='city' id='city'required//></td>
             </tr>
 
             <tr>
                 <td>State</td>
-                <td><input type='text' name='state' id='state'/></td>
+                <td><input type='text' name='state' id='state'required//></td>
             </tr>
 
             <tr>
                 <td>ZIP</td>
-                <td><input type='text' name='zip' id='zip'/></td>
+                <td><input type='text' name='zip' id='zip'required//></td>
             </tr>
 
             <tr>
                 <td>Phone Number</td>
-                <td><input type='text' name='phone' id='phone'/></td>
+                <td><input type='text' name='phone' id='phone'required//></td>
             </tr>
 
             <tr>
                 <td>Email</td>
-                <td><input type='text' name='email' id='email'/></td>
+                <td><input type='text' name='email' id='email'required//></td>
             </tr>
 
             <tr>
