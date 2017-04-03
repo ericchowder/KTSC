6f00020721634d764e45b4e0fab2e5e7e4955d2b
kevinkid135 <kevinkid228@hotmail.com>
2017-04-03 15:00:25 -0400

    Added Registration link to login page

--- 332Project/index.php
+++ 332Project/index.php
@@ -1,38 +1,48 @@
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <title>Welcome KTCS</title>
-  
-    </head>
+<head>
+    <title>Welcome KTCS</title>
+
+</head>
 <body>
 <style>
- #div
- {
-	width:40em;
-	margin-left:auto;
-	margin-right:auto;	 
- }
- </style>
+    #loginDiv {
+        width: 40em;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    #regDiv {
+        position: absolute;
+        right: 0;
+        top: 0;
+
+        padding: 5px;
+    }
+</style>
 <!-- dynamic content will be here -->
-<div id="myText" align='center'>
- <form name='login' id='login' action='index.php' method='post'>
-    <table border='0'>
-		<tr>
-            <td>Username</td>
-            <td><input type='text' name='username' id='username' /></td>
-        </tr>
-        <tr>
-            <td>Password</td>
-             <td><input type='password' name='password' id='password' /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <input type='submit' id='loginBtn' name='loginBtn' value='Log In' /> 
-            </td>
-        </tr>
-    </table>
-</form>
+<div id="loginDiv" align='center'>
+    <form name='login' id='login' action='index.php' method='post'>
+        <table border='0'>
+            <tr>
+                <td>Username</td>
+                <td><input type='text' name='username' id='username'/></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type='password' name='password' id='password'/></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type='submit' id='loginBtn' name='loginBtn' value='Log In'/>
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
+<div id="regDiv">
+    <a href="registration.php">Register</a>
 </div>
 
 </body>
