8561831697535981469428cde80f730e1da88914
kevinkid135 <kevinkid228@hotmail.com>
2017-04-06 14:11:20 -0400

    fixed errors

--- 332Project/adminPages/AdminHomePage.php
+++ 332Project/adminPages/AdminHomePage.php
@@ -4,36 +4,32 @@
     <title>HomePage</title>
     <link rel="stylesheet" type="text/css" href="../main.css">
 </head>
-<h1>Administrator Homepage.</h1>
 <body>
- <?php
-  //Create a user session or resume an existing one
- session_start();
- if($_SESSION){
- echo("Welcome back ");
- echo($_SESSION['name']);
- ?>
+<h1>Administrator Homepage.</h1>
+<?php
+//Create a user session or resume an existing one
+session_start();
+if ($_SESSION) {
+    echo("Welcome back ");
+    echo($_SESSION['name']);
+}
+?>
 
- 
- 
- <br><br>
+
+<br><br>
 <div class="corner">
-<form action="../index.php"  method="GET">
-    <input value='Logout' type='submit' id='logoutbutton' name='logoutbutton'/>
-</form>
-
+    <form action="../index.php" method="GET">
+        <input value='Logout' type='submit' id='logoutbutton' name='logoutbutton'>
+    </form>
 </div>
 
 <form action="memberList.php" method="GET">
-	<input value='Member List' type='submit' id='memberbutton' name='memberbutton'/>
+    <input value='Member List' type='submit' id='memberbutton' name='memberbutton'>
 </form>
 <br><br>
 <form action='carList.php' method="GET">
-	<input value='Car List' type='submit' id='carbutton' name='carbutton'/>
+    <input value='Car List' type='submit' id='carbutton' name='carbutton'>
 </form>
 <br><br>
-<form action='AvailCars.php'>
-<p>Reservations on <input type="date"> <input type="button" value="Search"></p>
 </body>
-</form>
 </html>
