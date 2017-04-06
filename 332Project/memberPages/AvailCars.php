9b15d0accce29c75374af7c7b6b7bcb660afc5a4
kevinkid135 <kevinkid228@hotmail.com>
2017-04-06 04:37:27 -0400

    fixed formatting errors

--- 332Project/memberPages/AvailCars.php
+++ 332Project/memberPages/AvailCars.php
@@ -1,4 +1,5 @@
-<HTML>
+<!DOCTYPE HTML>
+<html>
 <head>
     <link rel="stylesheet" type="text/css" href="../main.css">
 </head>
@@ -31,7 +32,7 @@ $result = mysqli_query($con, $query);
         echo "on ";
     echo $_GET['date'];
     ?>.
-    <h1>
+</h1>
 
 <!--Table Headers-->
 <table border="1">
@@ -41,78 +42,64 @@ $result = mysqli_query($con, $query);
         <th>Year</th>
         <th>City</th>
         <th>State</th>
-		<th>Rent</th>
+        <th>Rent</th>
     </tr>
-<?php
-	$counter=0;
-// populate table
- 		if(!$con)
-	{
-		die("Could not connect:".mysqli_error());
-	}
-while($row = mysqli_fetch_array($result)){
-	$counter++;
-	$vin=$row["vehicle_identification_number"];
-	
-	$date_requested = $_GET['date'];
-	
-	$qrt = "SELECT date,date_of_return 
+    <?php
+    $counter = 0;
+    // populate table
+    if (!$con) {
+        die("Could not connect:" . mysqli_error());
+    }
+    while ($row = mysqli_fetch_array($result)) {
+        $counter++;
+        $vin = $row["vehicle_identification_number"];
+
+        $date_requested = $_GET['date'];
+
+        $qrt = "SELECT date,date_of_return 
 	FROM reservations 
 	WHERE reservations.vehicle_identification_number=" . $vin;
 
-                $retval = mysqli_query($con, $qrt);
+        $retval = mysqli_query($con, $qrt);
 
-                if (!$retval) {
-                    die("Could not fetch data." . mysqli_error());
-                }
+        if (!$retval) {
+            die("Could not fetch data." . mysqli_error());
+        }
 
-                $col = mysqli_fetch_array($retval);
+        $col = mysqli_fetch_array($retval);
 
-                $date = $col['date'];
+        $date = $col['date'];
 
-                $date_of_return = $col['date_of_return'];
+        $date_of_return = $col['date_of_return'];
 
-                if ($date_requested > $date && $date_requested < $date_of_return) {
+        if ($date_requested > $date && $date_requested < $date_of_return) {
 
-                } else {
+        } else {
 
-    echo "<tr>\n";	
-	//echo "<td>" . $row["vehicle_identification_number"]. "</td>\n";
-	echo "<td>" . $row["make"] . "</td>\n";
-	echo "<td>" . $row["model"] . "</td>\n";
-	echo "<td>" . $row["year"] . "</td>\n";
-	echo "<td>" . $row["city"] . "</td>\n";
-	echo "<td>" . $row["state"] . "</td>\n";
-	echo "<td>" . "<input type='radio' name='select' id='selectcar'
-	checked='checked' onclick='setVal($counter{);'". "</td>\n";
-	}
+            echo "<tr>\n";
+            //echo "<td>" . $row["vehicle_identification_number"]. "</td>\n";
+            echo "<td>" . $row["make"] . "</td>\n";
+            echo "<td>" . $row["model"] . "</td>\n";
+            echo "<td>" . $row["year"] . "</td>\n";
+            echo "<td>" . $row["city"] . "</td>\n";
+            echo "<td>" . $row["state"] . "</td>\n";
+            echo "<td>" . "<input type='radio' name='select' id='selectcar'	checked='checked' onclick='setVal($counter{);'" . "</td>\n";
+        }
 
-	$_SESSION['VIN']=$row["vehicle_identification_number"];
+        $_SESSION['VIN'] = $row["vehicle_identification_number"];
 
-	}
-?>
-
-<div class="corner">
-<form action="../index.php" method="GET">
-	<input value='Logout' type='submit' id='logoutbutton' 
-	name='logoutbutton'/>
-</form>
-	<br>
-	<br>
-<form action="HomePage.php" method="GET">
-	<input value='HomePage' type='submit' id='homepage'
-	name='homepage'/>
-</form>
-</div>
+    }
+    ?>
 </table>
 <br>
 <form action="CarReservationPage.php" method="POST">
-	<input type="hidden" name="carBtn" id="carBtn">
-	<input type="submit" value="Make a reservation">
+    <input type="hidden" name="carBtn" id="carBtn">
+    <input type="submit" value="Make a reservation">
 </form>
 <script>
-	function setVal(c){
-		$('#memID').val($('#mem'+c).html());
-	}
+    function setVal(c) {
+        $('#memID').val($('#mem' + c).html());
+    }
+</script>
 </body>
 </html>
